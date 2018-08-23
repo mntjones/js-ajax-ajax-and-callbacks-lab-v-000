04d17fe42d6962af9e5f571eae0ec73d40005733
Monica Tamaru Jones <mntamaru@gmail.com>
2018-08-23 16:59:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-$(document).ready(function (){
+$(document).ready(function () {
+
 });
+
+function searchRepositories(){
+  // grabs the search terms entered from the html page
+    const searchTerms = document.getElementById("searchTerms").value;
+    
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, showResults).fail(displayError());
+}
+
+function showResults(repos) {
+  // for id = "results"
+  
+   $("#results").html(repos.items.map(repo => {
+        return (
+            `<h3>Name: ${repo.full_name}</h3>
+            <p>URL: ${repo.html_url}</p>
+            <p>Owner: ${repo.owner.login}</p>
+            <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p>`
+        );
+    }).join(""));
+}
+
+function showCommits(coms) {
+  $.get(`https://api.github.com/repos/${coms.dataset.owner}/${coms.dataset.repository}/commits`,
+  getCommits).fail(displayError);
+}
+
+function getCommits(display_com) {
+  $("#details").html(display_com.map(commit => {
+    return (
+      `<h4>${commit.sha}</h4>
+      <p>${commit.commit.message}</p>`
+      );
+  }).join(""));
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
